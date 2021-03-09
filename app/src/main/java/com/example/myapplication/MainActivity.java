3966aab3555cb26ef6f9049165c4748c84608bfa
Yu4n <28648731+Yu4n@users.noreply.github.com>
2021-03-09 18:00:20 +0800

    Task 2

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,25 +7,35 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.myapplication.databinding.ActivityMainBinding;
 
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding; //this variable has not been initialised
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button reverseButton = findViewById(R.id.reverseBtn);
-        reverseButton.setOnClickListener(new View.OnClickListener() {
+        // inflate() creates an instance of the binding class for the activity to use
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        // Get a reference to the root view
+        View view = binding.getRoot();
+        // The root view needs to be passed as an input to setContentView()
+        setContentView(view);
+        // setContentView(R.layout.activity_main);
+        binding.reverseBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText editText = findViewById(R.id.editMessage);
-                String builder= new StringBuilder(editText.getText()).reverse().toString();
-                editText.setText(builder);
+                // EditText editText = findViewById(R.id.editMessage);
+                String builder= new StringBuilder(binding.editMessage.getText()).reverse().toString();
+                binding.editMessage.setText(builder);
             }
         });
-        Button clearButton= findViewById(R.id.clearBtn);
-        clearButton.setOnClickListener(v -> {
-            EditText editText =findViewById(R.id.editMessage);
-            editText.setText("");
+        binding.clearBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // EditText editText =findViewById(R.id.editMessage);
+                binding.editMessage.setText("");
+            }
         });
+        //Button clearButton= findViewById(R.id.clearBtn);
     }
 }
